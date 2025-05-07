7c4aca125f078f4a0370fa41dd4d1e21314d7db2
Praneeth-Pike <praneeth@clearcut.design>
2025-05-08 01:36:06 +0530

    lint

--- src/straico-error.ts
+++ src/straico-error.ts
@@ -1,17 +1,17 @@
-import { createJsonErrorResponseHandler } from '@ai-sdk/provider-utils';
-import { z } from 'zod';
+import { createJsonErrorResponseHandler } from '@ai-sdk/provider-utils'
+import { z } from 'zod'
 
 const straicoErrorDataSchema = z.object({
-  object: z.literal('error'),
-  message: z.string(),
-  type: z.string(),
-  param: z.string().nullable(),
-  code: z.string().nullable(),
-});
+	object: z.literal('error'),
+	message: z.string(),
+	type: z.string(),
+	param: z.string().nullable(),
+	code: z.string().nullable(),
+})
 
-export type MistralErrorData = z.infer<typeof straicoErrorDataSchema>;
+export type StraicoErrorData = z.infer<typeof straicoErrorDataSchema>
 
 export const straicoFailedResponseHandler = createJsonErrorResponseHandler({
-  errorSchema: straicoErrorDataSchema,
-  errorToMessage: data => data.message,
-});
+	errorSchema: straicoErrorDataSchema,
+	errorToMessage: (data) => data.message,
+})
